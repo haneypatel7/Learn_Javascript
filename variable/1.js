8adc48a3df70cebfacdd98df10a8f87c2ea41695
haneypatel <haneypatel7@gmail.com>
2024-01-01 22:51:20 +0530

    teak

--- variable/1.js
+++ variable/1.js
@@ -5,6 +5,29 @@ var data=20;
 console.log(data);
 
 
+let fullname='haney';
+console.log(fullname);
+
+console.log(typeof(data));
+//below this is object
+// class student=
+// {
+      //  fullname:"rahul kumar"
+
+
+// };
+
 
 //here var variable is local variable types that can change any time declare next time at declareation
 
+//variable name is case sensetive in javascript
+//let cant re declared or updated
+//always use let for roubust code or output
+//premetive data type-number string boolean null symbol etc total 7
+
+
+//non premitive class object user define data types
+
+let lname='123';
+let result=lname+1;
+console.log(result);
---
+++ variable/product.js
@@ -0,0 +1,11 @@
+const product=
+{
+   title :"cello",
+   color :"black",
+   rating : 4,
+   price : 17,
+
+
+};
+
+console.log(product);
