6696b1c11172b26e57b6dd910d72a657e110dc98
haneypatel <haneypatel7@gmail.com>
2024-01-20 00:28:24 +0530

    map copied

---
+++ Array/map.js
@@ -0,0 +1,11 @@
+//manipulate values at every index at array
+ //using it we can copied the array values to other array
+
+
+ let array=[1,2,3,4,5];
+ let result=array.map((val)=>
+ {
+    return val;
+ });
+
+ console.log(result);
