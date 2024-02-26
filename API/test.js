a9e460d674d44cf71dfd4482dd30e20aeaf9be8c
haneypatel <haneypatel7@gmail.com>
2024-02-26 21:45:15 +0530

    api

--- API/test.js
+++ API/test.js
@@ -5,3 +5,14 @@
 //fetching resource by api provided inteface
 //status 200 success full fetch data
 
+let p=fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m');
+p.then((response)=>{
+    console.log(response.status);
+    return response.json();
+
+}).then ((value2)=>{
+    
+    console.log(value2);
+});
+
+console.log("hello");
--- function/aerrow.js
+++ function/aerrow.js
@@ -7,6 +7,16 @@ console.log(a);
 console.log(addval);
 
 
+ addsum=(a,b)=>
+{
+    return a+b;
+}
+
+var a=addsum(12,13);
+console.log(a);
+
+
+
 
 //above the example addvale act as a variable its not a funtion
 //heare fuction its without name
